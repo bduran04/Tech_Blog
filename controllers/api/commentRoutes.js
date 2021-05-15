bbfabc7bc6cd524744ad247afb07eb68505189e9
Belle Duran <bduran04@gmail.com>
2021-05-15 10:21:03 -0500

    updated the handlebars

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -3,6 +3,21 @@ const { Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 //router.get to locate all of the comment data
+router.get('/', async (req, res) => {
+    try {
+      const commentData = await Comment.findAll({
+      });
+  
+      if (!categoryData) {
+        res.status(404).json({ message: 'No categories found!' });
+        return;
+      }
+  
+      res.status(200).json(categoryData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 //router.post to allow comments for users who are loggin in (withAuth)
 
--- models/Post.js
+++ models/Post.js
@@ -1,17 +1,37 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Post extends Model {}
+class Post extends Model { }
 
 Post.init({
-    //this will need id, title, content, & user id as an object
-    //as well as below text
-    // {
-    //     sequelize,
-    //     freezeTableName: true,
-    //     underscored: true,
-    //     modelName: 'post',
-    //   }
-});
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    content: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'user',
+            key: 'id'
+        }
+    }
+},
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post',
+    }
+);
 
 module.exports = Post;
